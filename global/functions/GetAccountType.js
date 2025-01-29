493e26e30d361e1954ab45f7cdbd1bd20c00390e
golash0000 <obsequio.kenneth.ampoloquio@gmail.com>
2025-01-30 03:19:54 +0800

    ENV

--- global/functions/GetAccountType.js
+++
@@ -1,7 +0,0 @@
-const GetAccountType = (type) => {
-    const type_dict = { resident: "Resident", staff: "Barangay Staff", brgy_admin: "Barangay Admin", municipality_admin: "Head Admin",}
-
-    return type_dict[type];
-};
-
-module.exports = GetAccountType;
