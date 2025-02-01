f98e9e3358b6786f762282976bb88c29944a9adb
golash0000 <obsequio.kenneth.ampoloquio@gmail.com>
2025-02-01 13:43:41 +0800

    profile created

--- src/applicant/account_login/account_login.route.js
+++ src/applicant/account_login/account_login.route.js
@@ -11,7 +11,7 @@ const upload = require("../../../global/config/Multer");
 const RequireAuth = require("../../../global/middleware/RequireAuth");
 
 router.post("/login", Login);
-router.post("/refresh", Refresh);
+router.post("/refresh", RequireAuth, Refresh);
 router.post("/register_applicant", RegisterApplicant);
 
 module.exports = router;
