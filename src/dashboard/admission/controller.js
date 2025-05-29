eabe0da5df287bf840991e4e8cb7897bd43c0874
Rogu <obsequio.rus@gmail.com>
2025-05-29 18:47:56 +0800

    Comment out console log in AdmissionDashboardController

--- src/dashboard/admission/controller.js
+++ src/dashboard/admission/controller.js
@@ -129,7 +129,7 @@ const AdmissionDashboardController = {
         transfereeCVSU: formatCounts(userCounts[0].transfereeCVSU),
       };
 
-      console.log(response);
+      // console.log(response);
       res.status(200).json(response);
     } catch (error) {
       res.status(400).json({ message: "Error fetching dashboard counts", error: error.message });
